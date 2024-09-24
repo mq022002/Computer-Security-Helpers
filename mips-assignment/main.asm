1520862d0f9a4b3d70f3e1ffff705287fd1f2345
Matthew Quijano <mq022002@gmail.com>
2024-09-24 00:52:58 -0400

    refactor: Convert label for displaying remainder to MIPS

--- mips-assignment/main.asm
+++ mips-assignment/main.asm
@@ -94,8 +94,9 @@ print_results:
     la      $a0,            newline
     syscall 
 
-    li      $v0,            4
-    la      $a0,            label_rem
+    addi    $v0,            $zero,          4
+    lui     $a0,            0x1001
+    ori     $a0,            $a0,            0x0063
     syscall 
     lw      $a0,            data_mem+12
     li      $v0,            1
