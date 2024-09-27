85037d789d853096cb1ccd71f85f4f7db222718c
Matthew Quijano <mq022002@gmail.com>
2024-09-27 09:43:44 -0400

    feat: CBC encryption integrated into MainProgram
    - Test case passed: 123456789abcdefd456789aca32eb1fd456789aca32eb1fd456789aca32eb1f -> a5198801e1502b32bb0a8c3e94147245de54425b15ad4b7dcdd596b67851bc81

--- block-cipher-encryption/src/MainProgram.java
+++ block-cipher-encryption/src/MainProgram.java
@@ -88,6 +88,26 @@ public class MainProgram {
         return ciphertext;
     }
 
+    public static byte[] cbcEncrypt(String iv, byte[] plaintext) {
+        byte[] ciphertext = new byte[plaintext.length];
+        String previousCipher = iv;
+        for (int i = 0; i < plaintext.length; i++) {
+            byte currentByte = plaintext[i];
+            String highNibble = String.format("%4s", Integer.toBinaryString((currentByte >> 4) & 0x0F)).replace(' ',
+                    '0');
+            String lowNibble = String.format("%4s", Integer.toBinaryString(currentByte & 0x0F)).replace(' ', '0');
+            String xoredHigh = xorBits(previousCipher, highNibble);
+            String substitutedHigh = substitute(xoredHigh);
+            previousCipher = substitutedHigh;
+            String xoredLow = xorBits(previousCipher, lowNibble);
+            String substitutedLow = substitute(xoredLow);
+            previousCipher = substitutedLow;
+            String combined = substitutedHigh + substitutedLow;
+            ciphertext[i] = (byte) Integer.parseInt(combined, 2);
+        }
+        return ciphertext;
+    }
+
     private static String xorBits(String a, String b) {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < 4; i++) {
@@ -112,13 +132,16 @@ public class MainProgram {
             scanner.close();
             return;
         }
+        String ivBinary = String.format("%4s", Integer.toBinaryString(Integer.parseInt(ivHex, 16))).replace(' ', '0');
 
         byte[] ecbCipher = ecbEncrypt(plaintext);
         byte[] ctrCipher = ctrEncrypt(ivHex, plaintext);
+        byte[] cbcCipher = cbcEncrypt(ivBinary, plaintext);
 
         System.out.println("Plaintext: " + plaintextHex.toLowerCase());
         System.out.println("ECB Ciphertext: " + byteArrayToHexString(ecbCipher).toLowerCase());
         System.out.println("CTR Ciphertext: " + byteArrayToHexString(ctrCipher).toLowerCase());
+        System.out.println("CBC Ciphertext: " + byteArrayToHexString(cbcCipher).toLowerCase());
 
         scanner.close();
     }
