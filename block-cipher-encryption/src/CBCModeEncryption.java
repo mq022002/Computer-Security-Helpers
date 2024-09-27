39ef7ad7a1ddeff9d040fb5d0d664122345dc25e
Matthew Quijano <mq022002@gmail.com>
2024-09-27 10:08:23 -0400

    refactor: Move cbcEncrypt to a separate file

--- block-cipher-encryption/src/CBCModeEncryption.java
+++ block-cipher-encryption/src/CBCModeEncryption.java
@@ -1,3 +1,32 @@
 public class CBCModeEncryption {
-    
+
+    public static byte[] cbcEncrypt(String iv, byte[] plaintext) {
+        byte[] ciphertext = new byte[plaintext.length];
+        String previousCipher = iv;
+        for (int i = 0; i < plaintext.length; i++) {
+            byte currentByte = plaintext[i];
+            String highNibble = String.format("%4s", Integer.toBinaryString((currentByte >> 4) & 0x0F)).replace(' ',
+                    '0');
+            String lowNibble = String.format("%4s", Integer.toBinaryString(currentByte & 0x0F)).replace(' ', '0');
+            String xoredHigh = xorBits(previousCipher, highNibble);
+            String substitutedHigh = SubstitutionCipher.substitute(xoredHigh);
+            previousCipher = substitutedHigh;
+            String xoredLow = xorBits(previousCipher, lowNibble);
+            String substitutedLow = SubstitutionCipher.substitute(xoredLow);
+            previousCipher = substitutedLow;
+            String combined = substitutedHigh + substitutedLow;
+            ciphertext[i] = (byte) Integer.parseInt(combined, 2);
+        }
+        return ciphertext;
+    }
+
+    private static String xorBits(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 4; i++) {
+            char bitA = a.charAt(i);
+            char bitB = b.charAt(i);
+            sb.append(bitA == bitB ? '0' : '1');
+        }
+        return sb.toString();
+    }
 }
--- block-cipher-encryption/src/MainProgram.java
+++ block-cipher-encryption/src/MainProgram.java
@@ -24,36 +24,6 @@ public class MainProgram {
         return sb.toString();
     }
 
-    public static byte[] cbcEncrypt(String iv, byte[] plaintext) {
-        byte[] ciphertext = new byte[plaintext.length];
-        String previousCipher = iv;
-        for (int i = 0; i < plaintext.length; i++) {
-            byte currentByte = plaintext[i];
-            String highNibble = String.format("%4s", Integer.toBinaryString((currentByte >> 4) & 0x0F)).replace(' ',
-                    '0');
-            String lowNibble = String.format("%4s", Integer.toBinaryString(currentByte & 0x0F)).replace(' ', '0');
-            String xoredHigh = xorBits(previousCipher, highNibble);
-            String substitutedHigh = SubstitutionCipher.substitute(xoredHigh);
-            previousCipher = substitutedHigh;
-            String xoredLow = xorBits(previousCipher, lowNibble);
-            String substitutedLow = SubstitutionCipher.substitute(xoredLow);
-            previousCipher = substitutedLow;
-            String combined = substitutedHigh + substitutedLow;
-            ciphertext[i] = (byte) Integer.parseInt(combined, 2);
-        }
-        return ciphertext;
-    }
-
-    private static String xorBits(String a, String b) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < 4; i++) {
-            char bitA = a.charAt(i);
-            char bitB = b.charAt(i);
-            sb.append(bitA == bitB ? '0' : '1');
-        }
-        return sb.toString();
-    }
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -72,7 +42,7 @@ public class MainProgram {
 
         byte[] ecbCipher = ECBModeEncryption.ecbEncrypt(plaintext);
         byte[] ctrCipher = CTRModeEncryption.ctrEncrypt(ivHex, plaintext);
-        byte[] cbcCipher = cbcEncrypt(ivBinary, plaintext);
+        byte[] cbcCipher = CBCModeEncryption.cbcEncrypt(ivBinary, plaintext);
 
         System.out.println("Plaintext: " + plaintextHex.toLowerCase());
         System.out.println("ECB Ciphertext: " + byteArrayToHexString(ecbCipher).toLowerCase());
