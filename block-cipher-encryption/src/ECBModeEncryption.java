ab549aaecbc5e419784a9a8339f952bdadc89651
Matthew Quijano <mq022002@gmail.com>
2024-09-27 09:56:49 -0400

    refactor: Move ecbEncrypt to a separate file

--- block-cipher-encryption/src/ECBModeEncryption.java
+++ block-cipher-encryption/src/ECBModeEncryption.java
@@ -1,3 +1,17 @@
 public class ECBModeEncryption {
-    
+
+    public static byte[] ecbEncrypt(byte[] plaintext) {
+        byte[] ciphertext = new byte[plaintext.length];
+        for (int i = 0; i < plaintext.length; i++) {
+            byte currentByte = plaintext[i];
+            String highNibble = String.format("%4s", Integer.toBinaryString((currentByte >> 4) & 0x0F)).replace(' ',
+                    '0');
+            String lowNibble = String.format("%4s", Integer.toBinaryString(currentByte & 0x0F)).replace(' ', '0');
+            String substitutedHigh = SubstitutionCipher.substitute(highNibble);
+            String substitutedLow = SubstitutionCipher.substitute(lowNibble);
+            String combined = substitutedHigh + substitutedLow;
+            ciphertext[i] = (byte) Integer.parseInt(combined, 2);
+        }
+        return ciphertext;
+    }
 }
--- block-cipher-encryption/src/MainProgram.java
+++ block-cipher-encryption/src/MainProgram.java
@@ -24,21 +24,6 @@ public class MainProgram {
         return sb.toString();
     }
 
-    public static byte[] ecbEncrypt(byte[] plaintext) {
-        byte[] ciphertext = new byte[plaintext.length];
-        for (int i = 0; i < plaintext.length; i++) {
-            byte currentByte = plaintext[i];
-            String highNibble = String.format("%4s", Integer.toBinaryString((currentByte >> 4) & 0x0F)).replace(' ',
-                    '0');
-            String lowNibble = String.format("%4s", Integer.toBinaryString(currentByte & 0x0F)).replace(' ', '0');
-            String substitutedHigh = SubstitutionCipher.substitute(highNibble);
-            String substitutedLow = SubstitutionCipher.substitute(lowNibble);
-            String combined = substitutedHigh + substitutedLow;
-            ciphertext[i] = (byte) Integer.parseInt(combined, 2);
-        }
-        return ciphertext;
-    }
-
     public static byte[] ctrEncrypt(String iv, byte[] plaintext) {
         byte[] ciphertext = new byte[plaintext.length];
         int counter = Integer.parseInt(iv, 16) & 0x0F;
@@ -110,7 +95,7 @@ public class MainProgram {
         }
         String ivBinary = String.format("%4s", Integer.toBinaryString(Integer.parseInt(ivHex, 16))).replace(' ', '0');
 
-        byte[] ecbCipher = ecbEncrypt(plaintext);
+        byte[] ecbCipher = ECBModeEncryption.ecbEncrypt(plaintext);
         byte[] ctrCipher = ctrEncrypt(ivHex, plaintext);
         byte[] cbcCipher = cbcEncrypt(ivBinary, plaintext);
 
